98770745960bd2e7e93ba2afa41938af6eeb16b6
Rubenvandeven01 <rubenvandeven01@gmail.com>
2021-04-20 13:26:58 +0200

    Logo's static for desktop

--- components/Referrals.vue
+++ components/Referrals.vue
@@ -1,19 +1,27 @@
 <template>
   <section class="referrals bg-white">
     <client-only>
-      <div class="referrals__wrapper">
+      <div class="referrals__wrapper" :class='{ "no-slider": !slider }'>
         <div class="referrals__stars">
           <i v-for="rating in activeElement.rating" :key="rating" class="icon icon--star"></i>
         </div>
-        <div class='content-section'>
+        <div class="content-section">
           <span class="referrals__title">"{{ activeElement.quote }}"</span>
 
-          <div class="referrals__logos">
+          <!-- When slider is active -->
+          <div v-if="slider" class="referrals__logos">
             <Slider :options="{ autoPlay: 2500, pageDots: false, prevNextButtons: false }">
               <div class="referrals__logo-wrapper" v-for="referral in referrals" :key="referral.id" :id="referral.id">
                 <img class="referrals__logo" :src="referral.logoUrl">
               </div>
             </Slider>
+          </div>
+
+          <!-- When slider is inactive -->
+          <div v-if="!slider" class="referrals__logos">
+            <div class="referrals__logo-wrapper" v-on:click="activeElement = referrals[referral.id];" :class='{ "is-selected": activeElement == referrals[referral.id] }' v-for="referral in referrals" :key="referral.id" :id="referral.id">
+              <img class="referrals__logo" :src="referral.logoUrl">
+            </div>
           </div>
         </div>
       </div>
@@ -25,6 +33,8 @@
 export default {
   data() {
     return {
+      slider: true,
+      console: console,
       referrals: [
         {
           id: 0,
@@ -43,7 +53,7 @@ export default {
         {
           id: 2,
           rating: 4,
-          quote: "We zien dat mensen speciaal komen voor de",
+          quote: "We zien dat mensen speciaal komen voor de schommeltafel.",
           logoUrl: "./images/referrals/logo_3.png",
           active: false
         },
@@ -73,6 +83,9 @@ export default {
   },
   mounted() {
     var elem = document.getElementsByClassName("referrals__logo-wrapper");
+    
+    document.getElementsByTagName("body")[0].onresize = this.bodyResize;
+    this.bodyResize(window);
 
     setInterval(() => {
       for (let index = 0; index < elem.length; index++) {
@@ -81,6 +94,22 @@ export default {
         }
       }
     }, 25);
+  },
+  methods: {
+    bodyResize(event) {
+      if (event.innerWidth == null) {
+        event = event.path[0];
+      }
+
+      if (event.innerWidth >= 1024) {
+        this.slider = false;
+      } else {
+        this.slider = true;
+      }
+    }
+  },
+  beforeMount() {
+    this.activeElement = this.referrals[0]
   }
 }
 </script>
